9f4e7c7a0c38d79eb5f9f94d711025225d075c87
rockhe168 <rockhe168@gmail.com>
2016-03-30 00:13:59 +0800

    delete file

--- NHibernate+Spring整合/Download/BeeOPOA/trunk/publish/readme.txt
+++
@@ -1,6 +0,0 @@
-直接运行BeeWebServerConsole.exe， 将独立运行一个以InnerWeb为站点的WebServer， 以方便您的预览。
-注意：端口号为固定为3324， 运行后， 自动打开网页http://localhost:3324/, 请点击index.htm。
-会想GAC中注册Bee.WebServer.dll， 该组件移植于Cassini， 代码未混淆， 可以查看。
-
-该平台基于Bee.Core.dll组件， 该组件已混淆， 但不妨碍平台的功能开发。
-
--- 博客园/GoodProject/MiniUI_project/miniui/czcraft/czcraft/js/nav.js
+++
@@ -1,9 +0,0 @@
-
-function setph(n){
-	var tli = document.getElementById("ph1").getElementsByTagName("a");
-	var mli = document.getElementById("ph2").getElementsByTagName("ul");
-	for(i=0;i<tli.length;i++){
-		tli[i].className=i==n?"ph_hover":"";
-		mli[i].style.display = i == n?"block":"none";
-	}
-}
